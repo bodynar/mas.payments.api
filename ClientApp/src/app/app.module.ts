13b8c073548aabe1415dba6eacd4d51cb60b8e11
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-07-05 14:27:58 +0300

    fix build

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app.routing';
+
 import { AppContainerComponent } from './component/appContainer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
+    AppContainerComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppContainerComponent]

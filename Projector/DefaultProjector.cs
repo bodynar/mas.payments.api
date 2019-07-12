2e390264c0fdc630e16fb4f4d05f2fafe5f41150
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-07-12 15:13:51 +0300

    add complex mapping to primitive model

--- Projector/DefaultProjector.cs
+++ Projector/DefaultProjector.cs
@@ -61,16 +61,28 @@ namespace MAS.Payments.Projector
 
                 try
                 {
-                    var destinationProperty =
-                            destinationProperties
-                                .FirstOrDefault(x =>
-                                    x.Name.ToLower() == propertyName
-                                    && x.PropertyType == sourceProperty.PropertyType);
-
-                    if (destinationProperty != null)
+                    if (sourceProperty.PropertyType.IsClass)
                     {
-                        var sourceValue = sourceProperty.GetValue(source);
-                        destinationProperty.SetValue(destination, sourceValue);
+                        var destinationComplexProperties =
+                            destinationProperties
+                                .Where(x => x.Name.ToLower().StartsWith(propertyName));
+
+                        if (destinationComplexProperties.Any())
+                            SetComplexProperties(source, destination, sourceProperty, destinationComplexProperties);
+                    }
+                    else
+                    {
+                        var destinationProperty =
+                                destinationProperties
+                                    .FirstOrDefault(x =>
+                                        x.Name.ToLower() == propertyName
+                                        && x.PropertyType == sourceProperty.PropertyType);
+
+                        if (destinationProperty != null)
+                        {
+                            var sourceValue = sourceProperty.GetValue(source);
+                            destinationProperty.SetValue(destination, sourceValue);
+                        }
                     }
                 }
                 catch (Exception)
@@ -80,6 +92,66 @@ namespace MAS.Payments.Projector
             }
 
             return destination;
+        }
+
+        private void SetComplexProperties(
+            TSource source, TDestination destination,
+            PropertyInfo property, IEnumerable<PropertyInfo> destinationComplexProperties
+        )
+        {
+            // todo: check
+
+            // PaymentType.ID
+            // PaymentType.Name
+            // PaymentTypeID
+            // PaymentTypeName
+
+            // propertyName: PaymentType
+            // destinationComplexProperties: [PaymentTypeID, PaymentTypeName]
+
+
+            // ---------------
+            // PaymentTypeCompanyName
+            // PaymentType.Company.Name
+
+            var sourcePropertyName = property.Name.ToLower();
+            var sourcePropertyValue = property.GetValue(source);
+
+            if (sourcePropertyValue != null)
+            {
+                var internalProperties = sourcePropertyValue.GetType().GetProperties();
+
+                foreach (var internalProperty in internalProperties)
+                {
+                    var internalPropertyName = internalProperty.Name.ToLower();
+
+                    if (internalProperty.PropertyType.IsClass)
+                    {
+                        var complexProperties =
+                            destinationComplexProperties
+                                .Where(x => x.Name.ToLower().StartsWith(internalPropertyName));
+
+                        if (destinationComplexProperties.Any())
+                            SetComplexProperties(source, destination, internalProperty, complexProperties);
+                    }
+                    else
+                    {
+                        var destinationPropertyNameSuggestion = $"{sourcePropertyName}{internalPropertyName}".ToLower();
+
+                        var exactDestinationProperty =
+                            destinationComplexProperties.FirstOrDefault(x => x.Name.ToLower() == destinationPropertyNameSuggestion);
+
+                        if (exactDestinationProperty != null)
+                        {
+                            var internalPropertyValue = internalProperty.GetValue(source);
+                            exactDestinationProperty.SetValue(destination, internalPropertyValue);
+                        }
+                    }
+
+
+                }
+            }
+
         }
 
         #endregion

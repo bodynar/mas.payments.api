c75096e56ed63b233ccb6017d360371af9bf1e3f
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-07-18 20:35:33 +0300

    update add payment type command

--- Commands/AddPaymentType/AddPaymentTypeCommandHandler.cs
+++ Commands/AddPaymentType/AddPaymentTypeCommandHandler.cs
@@ -1,19 +1,36 @@
 using MAS.Payments.DataBase;
+using MAS.Payments.DataBase.Access;
 using MAS.Payments.Infrastructure;
 using MAS.Payments.Infrastructure.Command;
+using MAS.Payments.Infrastructure.Exceptions;
+using MAS.Payments.Infrastructure.Extensions;
+using MAS.Payments.Infrastructure.Specification;
 
 namespace MAS.Payments.Commands
 {
     internal class AddPaymentTypeCommandHandler : BaseCommandHandler<AddPaymentTypeCommand>
     {
+        private IRepository<PaymentType> Repository { get; }
+
         public AddPaymentTypeCommandHandler(
             IResolver resolver
         ) : base(resolver)
         {
+            Repository = GetRepository<PaymentType>();
         }
 
         public override void Handle(AddPaymentTypeCommand command)
         {
+            var isUnique =
+                !Repository.GetAll().Any(
+                    new CommonSpecification<PaymentType>(x => x.Name == command.Name && x.Company == command.Company));
+
+            if (!isUnique)
+            {
+                throw new CommandExecutionException(
+                    $"Payment type with name {command.Name} from {command.Company} is already exist");
+            }
+
             var paymentType = new PaymentType
             {
                 Name = command.Name,
@@ -21,7 +38,7 @@ namespace MAS.Payments.Commands
                 Company = command.Company
             };
 
-            GetRepository<PaymentType>().Add(paymentType);
+            Repository.Add(paymentType);
         }
     }
 }

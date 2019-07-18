ae17cd40e61cf29e08d65d4b0609c03b5754176b
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-07-19 02:33:20 +0300

    fix clientApp build

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app.routing';
 import { CoreModule } from './core/core.module';
 
 import { AppContainerComponent } from './component/appContainer.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
+    AppContainerComponent,
+    HomeComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
+    AppRoutingModule,
     FormsModule,
     CoreModule
   ],

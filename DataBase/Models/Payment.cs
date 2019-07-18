421b2ad1c3c67ca5906889e78b81fd6095dff7c1
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-07-18 16:54:34 +0300

    add fk property to model

--- DataBase/Models/Payment.cs
+++ DataBase/Models/Payment.cs
@@ -10,6 +10,8 @@ namespace MAS.Payments.DataBase
 
         public string Description { get; set; }
 
+        public long PaymentTypeId { get; set; }
+
         public PaymentType PaymentType { get; set; }
     }
 }

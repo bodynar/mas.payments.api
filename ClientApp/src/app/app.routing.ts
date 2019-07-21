281b853ca29b8e4cf746ca2e703ea3242212532b
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-07-21 18:02:39 +0300

    update routing

--- ClientApp/src/app/app.routing.ts
+++ ClientApp/src/app/app.routing.ts
@@ -3,24 +3,41 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { MeasurementComponent } from './areas/measurement/component/measurement.component';
 import { PaymentsComponent } from './areas/payments/component/payments.component';
+import { AppComponent } from './components/app/app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './pages/notFound/notFound.component';
 
 const routes: Routes = [
     {
         path: '',
-        component: HomeComponent
+        redirectTo: 'app',
+        pathMatch: 'full'
     },
     {
-        path: 'payments',
-        component: PaymentsComponent
+        path: 'app',
+        component: AppComponent,
+        children: [
+            {
+                path: '',
+                component: HomeComponent
+            },
+            {
+                path: 'payments',
+                component: PaymentsComponent
+            },
+            {
+                path: 'measures',
+                component: MeasurementComponent
+            },
+        ]
     },
     {
-        path: 'measures',
-        component: MeasurementComponent
+        path: '404',
+        component: NotFoundComponent
     },
     {
         path: '**',
-        redirectTo: ''
+        redirectTo: 'pageNotFound'
     }
 ];
 
--- ClientApp/src/app/component/appContainer.component.ts
+++ ClientApp/src/app/component/appContainer.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'appContainer.template.pug',
-  styleUrls: ['appContainer.style.styl']
+  templateUrl: 'appContainer.template.pug'
 })
 class AppContainerComponent {
 

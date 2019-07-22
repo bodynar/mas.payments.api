db7d914d44a5fc07573020c3dd0c705f3fb6d93c
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-07-22 15:16:16 +0300

    add mockup notifications

--- ClientApp/src/app/areas/payments/component/payments.component.ts
+++ ClientApp/src/app/areas/payments/component/payments.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 
+import { INotificationService } from 'services/INotificationService';
+
 @Component({
     templateUrl: 'payments.template.pug',
     styleUrls: ['payments.style.styl']
 })
 class PaymentsComponent {
     constructor(
+        private notificationService: INotificationService
     ) {
+        setTimeout(() =>
+            this.notificationService.error('You don\'t have an permission to execute operation.'),
+            1 * 1000
+        );
+
+        setTimeout(() =>
+            this.notificationService.warning('Unauthorized persons will be executed.'),
+            1 * 3000
+        );
+
+        setTimeout(() =>
+            this.notificationService.success('Your application have been accepted.'),
+            1 * 5000
+        );
+
+        setTimeout(() =>
+            this.notificationService.info('Remote databases has been restored.'),
+            1 * 7000
+        );
     }
 }
 

93caea7a129dbb2a4dcaec05b4930862350e063c
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-07-26 02:02:12 +0300

    remove models

--- ClientApp/src/app/core/models/meterMeasurementType.ts
+++
@@ -1,9 +0,0 @@
-interface MeterMeasurementType {
-    id: number;
-    name: string;
-    paymentType: string;
-    paymentTypeId: number;
-    description?: string;
-}
-
-export { MeterMeasurementType };
--- ClientApp/src/app/core/models/paymentType.ts
+++
@@ -1,8 +0,0 @@
-interface PaymentType {
-    id: number;
-    name: string;
-    description?: string;
-    company?: string;
-}
-
-export { PaymentType };

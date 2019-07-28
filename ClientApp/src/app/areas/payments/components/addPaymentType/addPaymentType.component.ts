b60361a3ec029dab235b13434eb38fc61a3f557e
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-07-28 16:09:31 +0300

    add addPayment component

--- ClientApp/src/app/areas/payments/components/addPaymentType/addPaymentType.component.ts
+++ ClientApp/src/app/areas/payments/components/addPaymentType/addPaymentType.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { ReplaySubject, Subject } from 'rxjs';
+import { filter, switchMap, takeUntil } from 'rxjs/operators';
+
+import { INotificationService } from 'services/INotificationService';
+import { IPaymentService } from 'services/IPaymentService';
+import { IRouterService } from 'services/IRouterService';
+
+import { AddPaymentTypeRequest } from 'models/request/addPaymentTypeRequest';
 
 @Component({
     templateUrl: 'addPaymentType.template.pug',
     styleUrls: ['addPaymentType.style.styl']
 })
-class AddPaymentTypeComponent {
+class AddPaymentTypeComponent implements OnDestroy {
+
+    public addPaymentTypeRequest: AddPaymentTypeRequest =
+        {
+            name: ''
+        };
+
+    public whenSubmittedForm$: Subject<NgForm> =
+        new ReplaySubject(1);
+
+    private whenComponentDestroy$: Subject<null> =
+        new Subject();
+
     constructor(
-    ) { }
+        private paymentService: IPaymentService,
+        private notificationService: INotificationService,
+        private routerService: IRouterService,
+    ) {
+        this.whenSubmittedForm$
+            .pipe(
+                takeUntil(this.whenComponentDestroy$),
+                filter(({ valid }) => valid),
+                switchMap(_ => this.paymentService.addPaymentType(this.addPaymentTypeRequest)),
+                filter(hasError => {
+                    if (hasError) {
+                        this.notificationService.error('Error due saving data. Please, try again later');
+                    } else {
+                        this.notificationService.success('Payment type was successfully added.');
+                    }
+
+                    return !hasError;
+                })
+            )
+            .subscribe(_ => this.routerService.navigateUp());
+    }
+
+    public ngOnDestroy(): void {
+        this.whenComponentDestroy$.next(null);
+        this.whenComponentDestroy$.complete();
+    }
+
+    public onFormSubmit(form: NgForm): void {
+        this.whenSubmittedForm$.next(form);
+    }
 }
 
 export { AddPaymentTypeComponent };

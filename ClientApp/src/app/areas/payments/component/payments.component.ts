f9cbabc281adb10aaa670d57ada018202eef98af
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-07-28 21:27:20 +0300

    remove mock notifcations

--- ClientApp/src/app/areas/payments/component/payments.component.ts
+++ ClientApp/src/app/areas/payments/component/payments.component.ts
@@ -1,33 +1,11 @@
 import { Component } from '@angular/core';
 
-import { INotificationService } from 'services/INotificationService';
-
 @Component({
     templateUrl: 'payments.template.pug'
 })
 class PaymentsComponent {
     constructor(
-        private notificationService: INotificationService
     ) {
-        setTimeout(() =>
-            this.notificationService.error('You don\'t have an permission to execute operation.'),
-            1 * 1000
-        );
-
-        setTimeout(() =>
-            this.notificationService.warning('Unauthorized persons will be executed.'),
-            1 * 3000
-        );
-
-        setTimeout(() =>
-            this.notificationService.success('Your application have been accepted.'),
-            1 * 5000
-        );
-
-        setTimeout(() =>
-            this.notificationService.info('Remote databases has been restored.'),
-            1 * 7000
-        );
     }
 }
 

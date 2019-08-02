e570d2f4e2d3e5b0c5ed20a11265af206d3c356e
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-08-02 12:15:12 +0300

    add statistics controller

---
+++ Controllers/StatisticsApiController.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using MAS.Payments.Infrastructure.Command;
+using MAS.Payments.Infrastructure.Query;
+using MAS.Payments.Models;
+using MAS.Payments.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MAS.Payments.Controllers
+{
+    [Route("api/stats")]
+    public class StatisticsApiController : BaseApiController
+    {
+        public StatisticsApiController(
+            ICommandProcessor commandProcessor,
+            IQueryProcessor queryProcessor
+        ) : base(commandProcessor, queryProcessor)
+        {
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<GetStatisticsResponse> GetStatistics([FromQuery]GetStatisticsRequest request)
+        {
+            GetStatisticsQuery query = null;
+
+            if (request.Year.HasValue)
+            {
+                query = new GetStatisticsQuery(request.Year.Value, request.IncludeMeasurements);
+            }
+            else if (request.IsDatePeriodSpecified)
+            {
+                query = new GetStatisticsQuery(request.From.Value, request.To.Value, request.IncludeMeasurements);
+            }
+            else
+            {
+                query = new GetStatisticsQuery(request.IncludeMeasurements);
+            }
+
+            return QueryProcessor.Execute(query);
+        }
+    }
+}

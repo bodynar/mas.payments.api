5431f7f4590221bbb21c3baf1b1c4c5d8eb46bde
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-08-04 03:03:21 +0300

    update home components for areas

--- ClientApp/src/app/areas/measurement/component/measurement.component.ts
+++ ClientApp/src/app/areas/measurement/component/measurement.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 
+import { IRouterService } from 'services/IRouterService';
+
 @Component({
-    templateUrl: 'measurement.template.pug'
+    templateUrl: 'measurement.template.pug',
+    styleUrls: ['measurement.style.styl']
 })
 class MeasurementsComponent {
     constructor(
-    ) { }
+        private routerService: IRouterService
+    ) {
+    }
+
+    public onModuleNameClick(): void {
+        this.routerService.navigate(['app', 'measurements']);
+    }
 }
 
 export { MeasurementsComponent };
--- ClientApp/src/app/areas/payments/component/payments.component.ts
+++ ClientApp/src/app/areas/payments/component/payments.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
+import { IRouterService } from 'services/IRouterService';
+
 @Component({
-    templateUrl: 'payments.template.pug'
+    templateUrl: 'payments.template.pug',
+    styleUrls: ['payments.style.styl']
 })
 class PaymentsComponent {
     constructor(
+        private routerService: IRouterService
     ) {
+    }
+
+    public onModuleNameClick(): void {
+        this.routerService.navigate(['app', 'payments']);
     }
 }
 

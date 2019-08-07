1b6b942ecfbdbc4c4666bed9f0807473658a28e4
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-08-07 21:55:47 +0300

    add test notification

---
+++ Notifications/TestNotificator.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using MAS.Payments.Infrastructure;
+
+namespace MAS.Payments.Notifications
+{
+    internal class TestNotificator : BaseNotificator
+    {
+        public TestNotificator(
+            IResolver resolver
+        ) : base(resolver)
+        {
+        }
+
+        public override IEnumerable<Notification> GetNotifications()
+        {
+            yield return new Notification
+            {
+                Name = " Test ",
+                Description = " lorem ipsum ",
+                Type = NotificationType.Info
+            };
+        }
+    }
+}

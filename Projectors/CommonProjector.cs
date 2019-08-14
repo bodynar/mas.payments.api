71e9be8a0efa8ef89d6d48824eb61e1d0d6f1119
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-08-14 20:00:37 +0300

    add common projector

---
+++ Projectors/CommonProjector.cs
@@ -0,0 +1,47 @@
+using System.Linq.Expressions;
+using System;
+using System.Collections.Generic;
+using MAS.Payments.Infrastructure.Projector;
+
+namespace MAS.Payments.Projectors
+{
+    public partial class Projector
+    {
+        public class Common<TSource, TDestination> : IProjector<TSource, TDestination>
+            where TSource : class
+            where TDestination : class
+        {
+            private Func<TSource, TDestination> ProjectFuntion
+            {
+                get
+                {
+                    if (ProjectExpression == null)
+                    {
+                        throw new Exception($"Projector function isn't implemented");
+                    }
+
+                    return ProjectExpression.Compile();
+                }
+            }
+
+            private Expression<Func<TSource, TDestination>> ProjectExpression { get; }
+
+            public Common(Expression<Func<TSource, TDestination>> projectExpression)
+            {
+                ProjectExpression = projectExpression
+                    ?? throw new ArgumentException("Project expression is required");
+            }
+
+            public TDestination Project(TSource src)
+            {
+                return ProjectFuntion(src);
+            }
+
+            public IEnumerable<TDestination> Project(IEnumerable<TSource> src)
+            {
+                foreach (var item in src)
+                    yield return ProjectFuntion(item);
+            }
+        }
+    }
+}

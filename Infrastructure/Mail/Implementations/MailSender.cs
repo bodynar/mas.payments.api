07a16445bc4e26ae0f4aa6fb9cd9e9df5e0e4350
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-09-06 00:11:32 +0300

    Add mail members

---
+++ Infrastructure/Mail/Implementations/MailSender.cs
@@ -0,0 +1,22 @@
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace MAS.Payments.Infrastructure.MailMessaging
+{
+    public class MailSender : IMailSender
+    {
+        private ISmtpClientFactory Factory { get; }
+
+        public MailSender(ISmtpClientFactory factory)
+        {
+            Factory = factory;
+        }
+
+        public async Task SendMailAsync(MailMessage message)
+        {
+            var smtpClient = Factory.CreateSmtpClient();
+
+            await smtpClient.SendMailAsync(message);
+        }
+    }
+}

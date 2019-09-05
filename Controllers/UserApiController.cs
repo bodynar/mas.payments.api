a4e75bb339f498d24788ffe3988e86ad0e7b942e
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-09-06 01:22:39 +0300

    update sending test mail message

--- Controllers/UserApiController.cs
+++ Controllers/UserApiController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using MAS.Payments.Infrastructure.Command;
+using MAS.Payments.Infrastructure.MailMessaging;
 using MAS.Payments.Infrastructure.Query;
 using MAS.Payments.Models;
 using MAS.Payments.Notifications;
@@ -12,17 +14,27 @@ namespace MAS.Payments.Controllers
     [Route("api/user")]
     public class UserApiController : BaseApiController
     {
+        public IMailSender MailSender { get; }
+
+        public IMailBuilder MailBuilder { get; }
+
         public UserApiController(
             ICommandProcessor commandProcessor,
             IQueryProcessor queryProcessor,
-            INotificationProcessor notificationProcessor
+            INotificationProcessor notificationProcessor,
+            IMailSender mailSender,
+            IMailBuilder mailBuilder
         ) : base(commandProcessor, queryProcessor, notificationProcessor)
         {
+            MailSender = mailSender;
+            MailBuilder = mailBuilder;
         }
 
         [HttpGet("[action]")]
         public IEnumerable<GetNotificationsResponse> GetNotifications()
         {
+            TestMailMessage();
+
             return
                 NotificationProcessor
                     .GetNotifications()
@@ -32,6 +44,12 @@ namespace MAS.Payments.Controllers
                         Description = notification.Description,
                         Type = Enum.GetName(typeof(NotificationType), notification.Type)
                     });
+        }
+
+        [HttpPost("[action]")]
+        public Task TestMailMessage()
+        {
+            return MailSender.SendMailAsync(MailBuilder.FormTestMailMessage("bodynar@gmail.com"));
         }
     }
 }

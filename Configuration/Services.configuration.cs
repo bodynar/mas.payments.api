b410e81cfcda5ad171995df3c70162b1c2e9b2ba
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-09-06 00:12:09 +0300

    add services configuration

---
+++ Configuration/Services.configuration.cs
@@ -0,0 +1,49 @@
+using MAS.Payments.DataBase;
+using MAS.Payments.Infrastructure.MailMessaging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleInjector;
+
+namespace MAS.Payments.Configuration
+{
+    public static class ServicesConfiguration
+    {
+        public static void Configure(IServiceCollection services, IConfiguration configuration, Container container)
+        {
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(JsonConfiguration.Configure);
+
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(cfg =>
+            {
+                cfg.RootPath = "ClientApp/dist";
+            });
+
+            services.AddSimpleInjector(container, options =>
+            {
+                options.AddAspNetCore()
+                    .AddControllerActivation();
+            });
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<DataBaseContext>(options =>
+                options.UseSqlServer(connectionString, x => x.MigrationsAssembly("MAS.Payments"))
+                       .UseLazyLoadingProxies().UseSqlServer(connectionString)
+            );
+
+            #region Mail
+            
+            services.AddOptions();
+            services.Configure<SmtpSettings>(configuration.GetSection("SmtpSettings"));
+            services.AddSingleton<ISmtpClientFactory, SmtpClientFactory>();
+            services.AddSingleton<IMailSender, MailSender>();
+            
+            #endregion
+        }
+    }
+}

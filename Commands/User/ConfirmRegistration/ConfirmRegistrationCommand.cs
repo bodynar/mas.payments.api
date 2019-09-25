e4dd5ea2c036b500196e156bb670f1a58b592a6b
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-09-26 01:03:54 +0300

    add confirmRegistration command

---
+++ Commands/User/ConfirmRegistration/ConfirmRegistrationCommand.cs
@@ -0,0 +1,16 @@
+using System;
+
+using MAS.Payments.Infrastructure.Command;
+
+namespace MAS.Payments.Commands
+{
+    public class ConfirmRegistrationCommand : ICommand
+    {
+        public string Token { get; set; }
+
+        public ConfirmRegistrationCommand(string token)
+        {
+            Token = token ?? throw new ArgumentException(nameof(Token));
+        }
+    }
+}
---
+++ Commands/User/ConfirmRegistration/ConfirmRegistrationCommandHandler.cs
@@ -0,0 +1,37 @@
+using MAS.Payments.DataBase;
+using MAS.Payments.DataBase.Access;
+using MAS.Payments.Infrastructure;
+using MAS.Payments.Infrastructure.Command;
+using MAS.Payments.Infrastructure.Exceptions;
+using MAS.Payments.Infrastructure.Specification;
+
+namespace MAS.Payments.Commands
+{
+    internal class ConfirmRegistrationCommandHandler : BaseCommandHandler<RegisterUserCommand>
+    {
+        private IRepository<UserToken> Repository { get; }
+
+        public ConfirmRegistrationCommandHandler(
+            IResolver resolver
+        ) : base(resolver)
+        {
+            Repository = GetRepository<UserToken>();
+        }
+
+        public override void Handle(RegisterUserCommand command)
+        {
+            var userToken = Repository.Get(new CommonSpecification<UserToken>(x => x.Token == command.Token));
+
+            if (userToken != null)
+            {
+                userToken.User.IsActive = true;
+
+                Repository.Delete(userToken.Id);
+            }
+            else
+            {
+                throw new CommandExecutionException("Token not found");
+            }
+        }
+    }
+}

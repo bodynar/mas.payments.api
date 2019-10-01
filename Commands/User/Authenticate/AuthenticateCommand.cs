d984e50616c3fa89366ce81e683b36ec34a41de8
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-10-02 00:43:36 +0300

    add authenticate command

---
+++ Commands/User/Authenticate/AuthenticateCommand.cs
@@ -0,0 +1,23 @@
+using System;
+using MAS.Payments.Infrastructure.Command;
+
+namespace MAS.Payments.Commands
+{
+    public class AuthenticateCommand : ICommand
+    {
+        public string Login { get; }
+
+        public string PasswordHash { get; }
+
+        public bool RememberMe { get; }
+
+        public string Token { get; set; }
+
+        public AuthenticateCommand(string login, string passwordHash, bool rememberMe = false)
+        {
+            Login = login ?? throw new ArgumentNullException(nameof(Login));
+            PasswordHash = passwordHash ?? throw new ArgumentNullException(nameof(Login));
+            RememberMe = rememberMe;
+        }
+    }
+}
---
+++ Commands/User/Authenticate/AuthenticateCommandHandler.cs
@@ -0,0 +1,60 @@
+using System;
+using MAS.Payments.DataBase;
+using MAS.Payments.DataBase.Access;
+using MAS.Payments.Infrastructure;
+using MAS.Payments.Infrastructure.Command;
+using MAS.Payments.Infrastructure.Exceptions;
+using MAS.Payments.Infrastructure.Specification;
+
+namespace MAS.Payments.Commands
+{
+    internal class AuthenticateCommandHandler : BaseCommandHandler<AuthenticateCommand>
+    {
+        public IRepository<User> UserRepository { get; }
+
+        private IRepository<UserToken> UserTokenRepository { get; }
+
+        public AuthenticateCommandHandler(
+            IResolver resolver
+        ) : base(resolver)
+        {
+            UserRepository = GetRepository<User>();
+            UserTokenRepository = GetRepository<UserToken>();
+        }
+
+        public override void Handle(AuthenticateCommand command)
+        {
+            var user =
+                UserRepository.Get(
+                    new CommonSpecification<User>(
+                        x => x.Login == command.Login && x.PasswordHash == command.PasswordHash));
+
+            if (user == null)
+            {
+                throw new CommandExecutionException("User with specified data not found");
+            }
+
+            if (!user.IsActive)
+            {
+                throw new CommandExecutionException("User is not active");
+            }
+
+            var token = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+            var tokenLiveTime = command.RememberMe ? 24 : 12;
+
+            var authToken = new UserToken
+            {
+                CreatedAt = DateTime.UtcNow,
+                ActiveTo = DateTime.UtcNow.AddHours(tokenLiveTime),
+                UserTokenTypeId = (long)UserTokenTypeEnum.Auth,
+                User = user,
+                UserId = user.Id, 
+                Token = token
+            };
+
+            UserTokenRepository.Add(authToken);
+
+            command.Token = token;
+        }
+    }
+}

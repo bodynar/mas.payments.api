34bf735725d7214eeeef02db65231aaeef6128a7
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-10-13 13:00:03 +0300

    add isTokenValid query

---
+++ Queries/User/IsTokenValid/IsTokenValidQuery.cs
@@ -0,0 +1,25 @@
+using System;
+using MAS.Payments.DataBase;
+using MAS.Payments.Infrastructure.Query;
+
+namespace MAS.Payments.Queries
+{
+    public class IsTokenValidQuery : IQuery<bool>
+    {
+        public string Token { get; }
+
+        public UserTokenTypeEnum TokenTypeEnum { get; }
+
+        public long UserTokenTypeId
+            => (long)TokenTypeEnum;
+
+        public DateTime ActiveTo { get; }
+
+        public IsTokenValidQuery(string token, UserTokenTypeEnum tokenType)
+        {
+            Token = token;
+            TokenTypeEnum = tokenType;
+            ActiveTo = DateTime.Now;
+        }
+    }
+}
---
+++ Queries/User/IsTokenValid/IsTokenValidQueryHandler.cs
@@ -0,0 +1,32 @@
+using MAS.Payments.DataBase;
+using MAS.Payments.DataBase.Access;
+using MAS.Payments.Infrastructure;
+using MAS.Payments.Infrastructure.Query;
+using MAS.Payments.Infrastructure.Specification;
+
+namespace MAS.Payments.Queries
+{
+    internal class IsTokenValidQueryHandler : BaseQueryHandler<IsTokenValidQuery, bool>
+    {
+        private IRepository<UserToken> Repository { get; }
+
+        public IsTokenValidQueryHandler(
+            IResolver resolver
+        ) : base(resolver)
+        {
+            Repository = GetRepository<UserToken>();
+        }
+
+        public override bool Handle(IsTokenValidQuery query)
+        {
+            var token = 
+                Repository.Get(
+                    new CommonSpecification<UserToken>(
+                        x => x.Token == query.Token && x.UserTokenTypeId == query.UserTokenTypeId));
+
+            return 
+                token != null
+                && token.ActiveTo <= query.ActiveTo;
+        }
+    }
+}

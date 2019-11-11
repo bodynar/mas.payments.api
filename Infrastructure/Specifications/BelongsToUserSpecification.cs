815accd1b7cea82bdd043e0caa5522e193f3e0cb
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-11-11 13:42:07 +0300

    update belongsToUser specification

---
+++ Infrastructure/Specifications/BelongsToUserSpecification.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq.Expressions;
+using MAS.Payments.DataBase;
+
+namespace MAS.Payments.Infrastructure.Specification
+{
+    public class BelongsToUser<TEntity> : Specification<TEntity>
+        where TEntity : OwnedEntity<TEntity>
+    {
+        public long UserId { get; }
+        
+        public BelongsToUser(long userId)
+        {
+            UserId = userId;
+        }
+
+        public override Expression<Func<TEntity, bool>> IsSatisfied()
+        {
+            return x => x.GetOwnerId() == UserId;
+        }
+    }
+}

53d1441003f6257cbcc67622416886515e3406cb
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-11-11 13:41:13 +0300

    update ownedEntity interface

---
+++ DataBase/Models/Base/OwnedEntity.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq.Expressions;
+
+namespace MAS.Payments.DataBase
+{
+    public abstract class OwnedEntity<TEntity> : Entity
+        where TEntity : OwnedEntity<TEntity>
+    {
+        public abstract Expression<Func<TEntity, long>> EntityOwnerId { get; }
+
+        public long GetOwnerId()
+        {
+            return EntityOwnerId.Compile().Invoke((TEntity)this);
+        }
+    }
+}

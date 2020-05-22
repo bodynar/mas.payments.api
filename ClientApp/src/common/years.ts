c71fed3bd1b5fcbc52504eb504fdb2fad4ead436
bodynar <bodynar@gmail.com>
2020-05-22 20:55:49 +0300

    update client app misc

--- ClientApp/src/common/years.ts
+++ ClientApp/src/common/years.ts
@@ -1,21 +1,22 @@
 const years = function (start: number, end: number): Array<{ id?: number, name: string }> {
-  if (start < 0 || end < 0) {
-    throw new Error('Year range must be positive.');
-  }
+    if (start < 0 || end < 0) {
+        throw new Error('Year range must be positive.');
+    }
 
-  if (start > end) {
-    throw new Error('End year must be lesser than start year.');
-  }
+    if (start > end) {
+        throw new Error('End year must be lesser than start year.');
+    }
 
-  const tempArray = [];
+    const tempArray = [];
 
-  for (var i = start; i <= end; i++)
-    tempArray.push(i);
+    for (var i = start; i <= end; i++) {
+        tempArray.push(i);
+    }
 
-  return tempArray.map((item, index) => ({
-    id: item,
-    name: item
-  }));
-}
+    return tempArray.map((item, index) => ({
+        id: item,
+        name: item
+    }));
+};
 
 export { years };

9a826a9ecea701d9e9c7f7768adabd0c626f03e4
bodynar <bodynar@gmail.com>
2020-06-01 13:28:31 +0300

    #34 add userSetting utilities

---
+++ Utilities/UserSettingUtilities.cs
@@ -0,0 +1,23 @@
+﻿namespace MAS.Payments.Utilities
+{
+    using System;
+    using System.Linq;
+
+    using MAS.Payments.DataBase;
+
+    public static class UserSettingUtilities
+    {
+        public static T GetTypedSettingValue<T>(UserSettings userSetting)
+        {
+            var type = typeof(T);
+            var allowedTypes = Enum.GetNames(typeof(SettingDataValueType)).Select(x => x.ToLower());
+
+            if (!allowedTypes.Contains(type.Name.ToLower()))
+            {
+                throw new ArgumentException(string.Format("Type must be declared in [{0}] array.", allowedTypes));
+            }
+
+            return (T)Convert.ChangeType(userSetting.RawValue, type);
+        }
+    }
+}

3b629e6383fc41193a2e2437b907b628de60222d
bodynar <bodynar@gmail.com>
2020-06-17 18:23:39 +0300

    #59 update getNotifications api

--- Controllers/UserApiController.cs
+++ Controllers/UserApiController.cs
@@ -5,10 +5,10 @@ namespace MAS.Payments.Controllers
     using System.Linq;
 
     using MAS.Payments.Commands;
+    using MAS.Payments.DataBase;
     using MAS.Payments.Infrastructure;
     using MAS.Payments.MailMessages;
     using MAS.Payments.Models;
-    using MAS.Payments.Notifications;
     using MAS.Payments.Queries;
     using MAS.Payments.Utilities;
 
@@ -26,15 +26,20 @@ namespace MAS.Payments.Controllers
         [HttpGet("[action]")]
         public IEnumerable<GetNotificationsResponse> GetNotifications()
         {
+            var userNotifications = NotificationProcessor.GetNotifications();
+
+            if (userNotifications.Any())
+            {
+                CommandProcessor.Execute(new AddUserNotificationCommand(userNotifications));
+            }
+
             return
-                NotificationProcessor
-                    .GetNotifications()
-                    .Select(notification => new GetNotificationsResponse
-                    {
-                        Name = notification.Name,
-                        Description = notification.Description,
-                        Type = Enum.GetName(typeof(NotificationType), notification.Type)
-                    });
+                userNotifications.Select(notification => new GetNotificationsResponse
+                {
+                    Name = notification.Title,
+                    Description = notification.Text,
+                    Type = Enum.GetName(typeof(UserNotificationType), notification.Type)
+                });
         }
 
         [HttpPost("[action]")]

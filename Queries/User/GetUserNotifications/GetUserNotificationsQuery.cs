b92833fd46aa4e0bca2b9bba642ade05e9f47d53
bodynar <bodynar@gmail.com>
2020-06-18 16:15:16 +0300

    #59 add getUserNotifications query

---
+++ Queries/User/GetUserNotifications/GetUserNotificationsQuery.cs
@@ -0,0 +1,24 @@
+﻿namespace MAS.Payments.Queries
+{
+    using System.Collections.Generic;
+
+    using MAS.Payments.DataBase;
+    using MAS.Payments.Infrastructure.Query;
+
+    public enum GetUserNotificationsType
+    {
+        All = 1,
+        Visible = 2,
+        Hidden = 3,
+    }
+
+    public class GetUserNotificationsQuery: IQuery<IEnumerable<UserNotification>>
+    {
+        public GetUserNotificationsType NotificationsType { get; }
+
+        public GetUserNotificationsQuery(GetUserNotificationsType notificationsType)
+        {
+            NotificationsType = notificationsType;
+        }
+    }
+}
---
+++ Queries/User/GetUserNotifications/GetUserNotificationsQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿namespace MAS.Payments.Queries
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using MAS.Payments.DataBase;
+    using MAS.Payments.DataBase.Access;
+    using MAS.Payments.Infrastructure;
+    using MAS.Payments.Infrastructure.Query;
+    using MAS.Payments.Infrastructure.Specification;
+
+    internal class GetUserNotificationsQueryHandler : BaseQueryHandler<GetUserNotificationsQuery, IEnumerable<UserNotification>>
+    {
+        private IRepository<UserNotification> Repository { get; }
+
+        public GetUserNotificationsQueryHandler(
+            IResolver resolver
+        ) : base(resolver)
+        {
+            Repository = GetRepository<UserNotification>();
+        }
+
+        public override IEnumerable<UserNotification> Handle(GetUserNotificationsQuery query)
+        {
+            Specification<UserNotification> specification = new CommonSpecification<UserNotification>(x => true);
+
+            switch (query.NotificationsType)
+            {
+                case GetUserNotificationsType.Visible:
+                    specification = new CommonSpecification<UserNotification>(x => !x.IsHidden);
+                    break;
+                case GetUserNotificationsType.Hidden:
+                    specification = new CommonSpecification<UserNotification>(x => x.IsHidden);
+                    break;
+                default:
+                    break;
+            }
+
+            return Repository
+                .Where(specification)
+                .ToList();
+        }
+    }
+}

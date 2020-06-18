262a0295ca5de1011962dae7c0c2c93203257add
bodynar <bodynar@gmail.com>
2020-06-18 16:43:23 +0300

    #59 add hideUserNotification command

---
+++ Commands/User/HideUserNotification/HideUserNotificationCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace MAS.Payments.Commands
+{
+    using System.Collections.Generic;
+
+    using MAS.Payments.Infrastructure.Command;
+
+    public class HideUserNotificationCommand : ICommand
+    {
+        public IEnumerable<long> NotificationIds { get; }
+
+        public HideUserNotificationCommand(IEnumerable<long> notificationIds)
+        {
+            NotificationIds = new List<long>(notificationIds);
+        }
+
+        public HideUserNotificationCommand(long notificationId)
+        {
+            NotificationIds = new List<long>() { notificationId };
+        }
+    }
+}
---
+++ Commands/User/HideUserNotification/HideUserNotificationCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace MAS.Payments.Commands
+{
+    using MAS.Payments.DataBase;
+    using MAS.Payments.DataBase.Access;
+    using MAS.Payments.Infrastructure;
+    using MAS.Payments.Infrastructure.Command;
+
+    public class HideUserNotificationCommandHandler : BaseCommandHandler<HideUserNotificationCommand>
+    {
+        private IRepository<UserNotification> Repository { get; }
+
+        public HideUserNotificationCommandHandler(IResolver resolver)
+            : base(resolver)
+        {
+            Repository = GetRepository<UserNotification>();
+        }
+
+        public override void Handle(HideUserNotificationCommand command)
+        {
+            var notificationsToHide = 
+                Repository.Where(
+                    new CommonSpecification.IdIn<UserNotification>(command.NotificationIds)
+                );
+
+            foreach (var userNotification in notificationsToHide)
+            {
+                userNotification.IsHidden = true;
+                Repository.Update(userNotification.Id, userNotification);
+            }
+        }
+    }
+}

d8a18546da103c072063fe43d0e6217e36afc7d2
bodynar <bodynar@gmail.com>
2020-06-21 22:41:50 +0300

    #59 add userNotification component baseline

--- ClientApp/src/app/areas/user/components/notifications/userNotifications.component.ts
+++ ClientApp/src/app/areas/user/components/notifications/userNotifications.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { ReplaySubject, Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
+
+import { INotificationService } from 'services/INotificationService';
+import { IUserService } from 'services/IUserService';
+
+import GetNotificationsResponse from 'models/response/user/getNotificationsResponse';
 
 @Component({
     templateUrl: 'userNotifications.template.pug'
 })
-export class UserNotificationsComponent {
+export class UserNotificationsComponent implements OnInit, OnDestroy {
+
+    public notifications$: Subject<Array<GetNotificationsResponse>> =
+        new ReplaySubject(1);
+
+    private whenComponentDestroy$: Subject<null> =
+        new Subject();
+
+    private notifications: Array<GetNotificationsResponse> =
+        [];
+
     constructor(
+        private userService: IUserService,
+        private notificationService: INotificationService,
     ) {
+    }
+
+    public ngOnInit(): void {
+        this.userService
+            .getNotifications({ onlyActive: false })
+            .pipe(
+                takeUntil(this.whenComponentDestroy$),
+                filter(result => {
+                    if (!result.success) {
+                        this.notificationService.error(result.error);
+                    }
+
+                    return result.success;
+                })
+            )
+            .subscribe(({ result }) => {
+                this.notifications = result;
+                this.notifications$.next(this.notifications);
+            });
+    }
+
+    public ngOnDestroy(): void {
+        this.whenComponentDestroy$.next(null);
+        this.whenComponentDestroy$.complete();
     }
 }

7de9396e409cd56077eb5d161f619a767d092ffb
bodynar <bodynar@gmail.com>
2020-07-02 16:21:47 +0300

    #18 update mail processor

--- Infrastructure/Mail/Implementations/MailProcessor.cs
+++ Infrastructure/Mail/Implementations/MailProcessor.cs
@@ -2,7 +2,6 @@ namespace MAS.Payments.Infrastructure.MailMessaging
 {
     using System;
     using System.Net.Mail;
-    using System.Threading.Tasks;
 
     using MAS.Payments.Commands;
     using MAS.Payments.Infrastructure.Command;
@@ -10,11 +9,15 @@ namespace MAS.Payments.Infrastructure.MailMessaging
     public class MailProcessor : IMailProcessor
     {
         private readonly Lazy<IMailSender> mailSender;
+        private readonly Lazy<ICommandProcessor> commandProcessor;
 
         private IResolver Resolver { get; }
 
         private IMailSender MailSender
             => mailSender.Value;
+
+        private ICommandProcessor CommandProcessor
+            => commandProcessor.Value;
 
         public MailProcessor(
             IResolver resolver
@@ -22,28 +25,34 @@ namespace MAS.Payments.Infrastructure.MailMessaging
         {
             Resolver = resolver;
             mailSender = new Lazy<IMailSender>(() => Resolver.Resolve<IMailSender>());
+            commandProcessor = new Lazy<ICommandProcessor>(() => Resolver.Resolve<ICommandProcessor>());
         }
 
         public async void Send(IMailMessage message)
         {
-            dynamic builder = GetMailBuilder(message);
+            var builtMessage = BuildMailMessage(message: message);
 
-            var builtMessage = builder.Build((dynamic)message);
+            SaveMessageLogItem(builtMessage as MailMessage); // todo: re-design mail message components to store log items correctly
+
+            await MailSender.SendMailAsync(builtMessage).ConfigureAwait(true);
+        }
+
+        public async void Send<TModel>(IMailMessage<TModel> message)
+        {
+            var builtMessage = BuildMailMessage(messageWithModel: message as IMailMessage<object>);
 
             await MailSender.SendMailAsync(builtMessage).ConfigureAwait(true);
 
             SaveMessageLogItem(builtMessage as MailMessage);
         }
 
-        public async void Send<TModel>(IMailMessage<TModel> message)
+        private MailMessage BuildMailMessage(IMailMessage message = null, IMailMessage<object> messageWithModel = null)
         {
-            dynamic builder = GetMailBuilder(message);
+            dynamic builder = GetMailBuilder(message ?? messageWithModel);
 
-            var builtMessage = builder.Build((dynamic)message, message.Model);
+            var builtMessage = builder.Build((dynamic)(message ?? messageWithModel), (dynamic)messageWithModel?.Model);
 
-            await MailSender.SendMailAsync(builtMessage).ConfigureAwait(true);
-
-            SaveMessageLogItem(builtMessage as MailMessage);
+            return builtMessage;
         }
 
         private object GetMailBuilder<TMailMessage>(TMailMessage message)
@@ -60,9 +69,7 @@ namespace MAS.Payments.Infrastructure.MailMessaging
 
         private void SaveMessageLogItem(MailMessage mailMessage)
         {
-            var commandProcessor = Resolver.Resolve<ICommandProcessor>();
-
-            commandProcessor.Execute(
+            CommandProcessor.Execute(
                 new SaveMailMessageLogCommand(
                     mailMessage.To[0].Address, mailMessage.Subject, mailMessage.Body, DateTime.Now));
         }

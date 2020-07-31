e98b0c850b554e12318ea80ed58413bf045fc7ca
bodynar <bodynar@gmail.com>
2020-07-31 19:15:06 +0300

    #24 fix flat projector

--- Projectors/FlatProjector.cs
+++ Projectors/FlatProjector.cs
@@ -75,7 +75,22 @@ namespace MAS.Payments.Projectors
                                     .Where(x => x.Name.ToLower().StartsWith(propertyName));
 
                             if (destinationComplexProperties.Any())
+                            {
+                                if (destinationComplexProperties.Count() == 1)
+                                {
+                                    var destinationComplexProperty = destinationComplexProperties.First();
+                                    
+                                    if (destinationComplexProperty.PropertyType == sourceProperty.PropertyType
+                                        && destinationComplexProperty.Name.ToLower() == propertyName)
+                                    {
+                                        var sourceValue = sourceProperty.GetValue(source);
+                                        destinationComplexProperty.SetValue(destination, sourceValue);
+                                        continue;
+                                    }
+                                }
+
                                 SetComplexProperties(source, destination, sourceProperty, destinationComplexProperties, 1);
+                            }
                         }
                         else
                         {
@@ -167,7 +182,6 @@ namespace MAS.Payments.Projectors
                     typeof(float),
                     typeof(double),
                     typeof(decimal),
-                    typeof(DateTime),
                 };
             }
 

67b65154fb518d55eaddbef2bb59649f11f06ed9
bodynar <bodynar@gmail.com>
2020-08-20 18:20:26 +0300

    #25 update models, add migration

--- DataBase/Models/PaymentType.cs
+++ DataBase/Models/PaymentType.cs
@@ -5,16 +5,18 @@ namespace MAS.Payments.DataBase
     public class PaymentType : Entity
     {
         public string SystemName { get; set; }
-        
+
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Company { get; set; }
 
-        public virtual ICollection<MeterMeasurementType> MeasurementTypes { get; set; }
+        public string Color { get; set; }
 
-        public virtual ICollection<Payment> Payments { get; set; }
+        public virtual ICollection<MeterMeasurementType> MeasurementTypes { get; }
+
+        public virtual ICollection<Payment> Payments { get; }
 
         public PaymentType()
         {
---
+++ Migrations/20200820151736_AddColorToTypes.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MAS.Payments.Migrations
+{
+    public partial class AddColorToTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "PaymentType",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "MeterMeasurementType",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "PaymentType");
+
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "MeterMeasurementType");
+        }
+    }
+}

82b19cfc4a91c6fbe84f632db9e148d743af3060
bodynar <bodynar@gmail.com>
2020-08-25 22:30:23 +0300

    #93 add base component

--- ClientApp/src/app/common/components/BaseComponent.ts
+++ ClientApp/src/app/common/components/BaseComponent.ts
@@ -1,27 +1,28 @@
-import { OnDestroy, Component } from '@angular/core';
+import { OnDestroy, Component, OnInit } from '@angular/core';
 
 import { Subject } from 'rxjs';
-
-import { IRouterService } from 'services/IRouterService';
 
 @Component({
     template: ''
 })
-export default abstract class BaseComponent implements OnDestroy {
+export default abstract class BaseComponent implements OnDestroy, OnInit {
     protected whenComponentDestroy$: Subject<null> =
         new Subject();
 
+    protected whenComponentInit$: Subject<null> =
+        new Subject();
+
     constructor(
-        protected routerService: IRouterService
     ) {
+    }
+
+    public ngOnInit(): void {
+        this.whenComponentInit$.next(null);
+        this.whenComponentInit$.complete();
     }
 
     public ngOnDestroy(): void {
         this.whenComponentDestroy$.next(null);
         this.whenComponentDestroy$.complete();
-    }
-
-    public onBackClick(): void {
-        this.routerService.navigateBack();
     }
 }

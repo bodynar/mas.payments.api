2541bdd20adcd912a19e0fcab06da48975f47645
bodynar <bodynar@gmail.com>
2021-01-28 23:57:44 +0300

    #112 update chart name when new data arrives

--- ClientApp/src/app/areas/statistics/components/measurementStats/measurementStats.component.ts
+++ ClientApp/src/app/areas/statistics/components/measurementStats/measurementStats.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import { ApexAxisChartSeries, ApexTitleSubtitle, ApexXAxis } from 'ng-apexcharts';
 
 import { Subject } from 'rxjs';
 import { filter, switchMap, takeUntil, switchMapTo, delay, tap } from 'rxjs/operators';
 
-import { isNullOrUndefined } from 'common/utils/common';
+import { isNullOrEmpty, isNullOrUndefined } from 'common/utils/common';
 
 import BaseComponent from 'common/components/BaseComponent';
 
@@ -19,12 +19,18 @@ import MeasurementStatisticsFilter from 'models/request/stats/measurementStatist
 import MeasurementTypeResponse from 'models/response/measurements/measurementTypeResponse';
 import { GetMeasurementStatisticsResponse } from 'models/response/stats/measurementStatsResponse';
 import { MonthSelectorValue } from 'common/components/monthSelector/monthSelector.component';
+import { StatsChartComponent } from '../statsChart/statsChart.component';
 
 @Component({
     selector: 'app-stats-measurement',
     templateUrl: 'measurementStats.template.pug',
 })
 export class MeasurementStatsComponent extends BaseComponent {
+    private static chartName: string = 'Measurement statistics';
+
+    @ViewChild(StatsChartComponent, { static: false })
+    private chartElement: StatsChartComponent;
+
     public chart: {
         series: ApexAxisChartSeries,
         xaxis: ApexXAxis,
@@ -38,7 +44,7 @@ export class MeasurementStatsComponent extends BaseComponent {
                 ],
                 title: { text: 'Month' }
             },
-            title: { text: 'Measurement statistics' }
+            title: { text: MeasurementStatsComponent.chartName }
         };
 
     public measurementTypes: Array<MeasurementTypeResponse>
@@ -121,15 +127,21 @@ export class MeasurementStatsComponent extends BaseComponent {
                     ? ` to ${stats.to.toDateString()}` : ''
             );
 
+        if (!isNullOrUndefined(this.chartElement)) {
+            this.chartElement.setTitle(
+                MeasurementStatsComponent.chartName + (
+                    isNullOrEmpty(namePostfix)
+                        ? '' : namePostfix));
+        }
+
         if (hasAnyData) {
             this.chart.series = stats.typeStatistics.map(x => ({
-                name: `${x.measurementTypeName}${namePostfix}`,
+                name: `${x.measurementTypeName}`,
                 data: [...x.statisticsData.map(y => ({
-                    x: getMonthName(y.month),
+                    x: getMonthName(y.month - 1),
                     y: y.diff
                 }))]
-            })
-            );
+            }));
         } else {
             this.chart.series = [{
                 name: 'Empty',

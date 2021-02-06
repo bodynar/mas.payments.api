88de2980a53b785c497fe9e976a225fe074c8ba0
bodynar <bodynar@gmail.com>
2021-02-06 15:06:04 +0300

    #106 add payment group add api

---
+++ Commands/Payment/AddGroup/PaymentGroup.cs
@@ -0,0 +1,18 @@
+﻿namespace MAS.Payments.Commands
+{
+    public class PaymentGroup
+    {
+        public double Amount { get; }
+
+        public long PaymentTypeId { get; }
+
+        public string Description { get; }
+
+        public PaymentGroup(double amount, long paymentTypeId, string description)
+        {
+            Amount = amount;
+            PaymentTypeId = paymentTypeId;
+            Description = description;
+        }
+    }
+}

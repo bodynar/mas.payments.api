365d6c671ec9ff80777ecf0014a83b302bb2d343
bodynar <bodynar@gmail.com>
2021-02-20 17:43:12 +0300

    #117 make diff nullable column

---
+++ Migrations/20210220144240_Set measurement diff as nullable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MAS.Payments.Migrations
+{
+    public partial class Setmeasurementdiffasnullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Diff",
+                table: "MeterMeasurement",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Diff",
+                table: "MeterMeasurement",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+        }
+    }
+}

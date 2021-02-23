17b29d8caee0770f50074dec4735d7640bcc239e
bodynar <bodynar@gmail.com>
2021-02-23 19:42:20 +0300

    #117 update add measurement command

--- Commands/MeterMeasurement/AddGroup/AddMeasurementGroupCommandHandler.cs
+++ Commands/MeterMeasurement/AddGroup/AddMeasurementGroupCommandHandler.cs
@@ -31,35 +31,14 @@
                 return;
             }
 
-            // TODO: validate measurement
-
-            var notValidTypes =
-                command.Measurements
-                    .Where(x => MeterMeasurementTypeRepository.Get(x.MeasurementTypeId) == null)
-                    .Select(x => x.MeasurementTypeId);
-
-            if (notValidTypes.Any())
-            {
-                throw new CommandExecutionException(CommandType, $"Measurement types with ids [{string.Join(",", notValidTypes)}] doesn't exists");
-            }
+            Validate(command);
 
             var measurements = new List<MeterMeasurement>();
             var calculatedNewMeasurementDate = new DateTime(command.Date.Year, command.Date.Month, 20);
             var previousMonthDate = calculatedNewMeasurementDate.AddMonths(-1);
 
-            var measurementsOnSpecifiedMonth =
-                Repository.Where(new CommonSpecification<MeterMeasurement>(x => x.Date.Date == calculatedNewMeasurementDate));
-
             foreach (var measurementData in command.Measurements)
             {
-                var measurementWithSameType =
-                    measurementsOnSpecifiedMonth.FirstOrDefault(x => x.MeterMeasurementTypeId == measurementData.MeasurementTypeId);
-
-                if (measurementWithSameType != null)
-                {
-                    throw new ArgumentException($"Measurement for date {calculatedNewMeasurementDate:MMMM yyyy} with type \"{measurementWithSameType.MeasurementType.Name}\" is already added.");
-                }
-
                 var previousValue =
                     Repository.Where(new CommonSpecification<MeterMeasurement>(x =>
                         x.Date.Date.Month == previousMonthDate.Month
@@ -86,6 +65,67 @@
             }
 
             Repository.AddRange(measurements);
+        }
+
+        private void Validate(AddMeasurementGroupCommand command)
+        {
+            var notValidMeasurements = command.Measurements.Where(x => x.Measurement <= 0).Select(x => x.Measurement);
+
+            if (notValidMeasurements.Any())
+            {
+                throw new ArgumentException($"Cannot add measurements with value [{string.Join(", ", notValidMeasurements)}]. Value must be greater than 0.");
+            }
+
+            var hasDuplicateTypes =
+                new HashSet<long>(command.Measurements.Select(x => x.MeasurementTypeId)).Count != command.Measurements.Count();
+
+            if (hasDuplicateTypes)
+            {
+                throw new ArgumentException("Cannot add measurements with duplicate types.");
+            }
+
+            var notValidTypes =
+                command.Measurements
+                    .Where(x => MeterMeasurementTypeRepository.Get(x.MeasurementTypeId) == null)
+                    .Select(x => x.MeasurementTypeId);
+
+            if (notValidTypes.Any())
+            {
+                throw new CommandExecutionException(CommandType, $"Measurement types with ids [{string.Join(",", notValidTypes)}] doesn't exist.");
+            }
+
+            var calculatedNewMeasurementDate = new DateTime(command.Date.Year, command.Date.Month, 20);
+
+            var measurementsOnSpecifiedMonth =
+                Repository.Where(new CommonSpecification<MeterMeasurement>(x => x.Date.Date == calculatedNewMeasurementDate))
+                .ToList();
+
+            var duplicateTypes =
+                command.Measurements
+                    .Where(measurementData =>
+                        measurementsOnSpecifiedMonth.FirstOrDefault(x => x.MeterMeasurementTypeId == measurementData.MeasurementTypeId) != null)
+                    .Select(x => new {
+                        TypeName = MeterMeasurementTypeRepository.Get(x.MeasurementTypeId).Name,
+                        x.Measurement
+                    }).Select(x => $"{x.TypeName} - {x.Measurement}");
+
+            if (duplicateTypes.Any())
+            {
+                throw new ArgumentException($"Cannot add duplicate measurements for types which already exist for {calculatedNewMeasurementDate:MMMM yyyy}: {string.Join("; ", duplicateTypes)}.");
+            }
+
+            foreach (var measurementData in command.Measurements)
+            {
+                var previousTypeValue =
+                    Repository.Where(new CommonSpecification<MeterMeasurement>(x => x.MeterMeasurementTypeId == measurementData.MeasurementTypeId))
+                    .OrderByDescending(x => x.Date.Date)
+                    .FirstOrDefault();
+
+                if (measurementData.Measurement < previousTypeValue.Measurement)
+                {
+                    throw new ArgumentException($"Measurement value must be greater than previous. Cannot add value \"{measurementData.Measurement}\" that is less than previous \"{previousTypeValue.Measurement}\".");
+                }
+            }
         }
     }
 }

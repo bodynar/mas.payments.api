f9653055d72ff5b115e64d26b56d36103515abef
bodynar <bodynar@gmail.com>
2025-03-29 21:36:25 +0300

    update migrations for pgsql use & remove old entity

---
+++ Migrations/20250329175436_PostgreeInit.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MAS.Payments.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostgreeInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentType",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SystemName = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Company = table.Column<string>(type: "text", nullable: true),
+                    Color = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserNotification",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Text = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    HiddenAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsHidden = table.Column<bool>(type: "boolean", nullable: false),
+                    Type = table.Column<short>(type: "smallint", nullable: false),
+                    Key = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserNotification", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserSetting",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    DisplayName = table.Column<string>(type: "text", nullable: true),
+                    RawValue = table.Column<string>(type: "text", nullable: true),
+                    TypeName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSetting", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeterMeasurementType",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SystemName = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    PaymentTypeId = table.Column<long>(type: "bigint", nullable: false),
+                    Color = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeterMeasurementType", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MeterMeasurementType_PaymentType_PaymentTypeId",
+                        column: x => x.PaymentTypeId,
+                        principalTable: "PaymentType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payment",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Amount = table.Column<double>(type: "double precision", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    PaymentTypeId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payment_PaymentType_PaymentTypeId",
+                        column: x => x.PaymentTypeId,
+                        principalTable: "PaymentType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeterMeasurement",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Measurement = table.Column<double>(type: "double precision", nullable: false),
+                    Comment = table.Column<string>(type: "text", nullable: true),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsSent = table.Column<bool>(type: "boolean", nullable: false),
+                    Diff = table.Column<double>(type: "double precision", nullable: true),
+                    MeterMeasurementTypeId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeterMeasurement", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MeterMeasurement_MeterMeasurementType_MeterMeasurementTypeId",
+                        column: x => x.MeterMeasurementTypeId,
+                        principalTable: "MeterMeasurementType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserSetting",
+                columns: new[] { "Id", "DisplayName", "Name", "RawValue", "TypeName" },
+                values: new object[] { 2L, "Отображать уведомления по показаниям", "DisplayMeasurementsNotification", "true", "Boolean" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MeterMeasurement_MeterMeasurementTypeId",
+                table: "MeterMeasurement",
+                column: "MeterMeasurementTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MeterMeasurementType_PaymentTypeId",
+                table: "MeterMeasurementType",
+                column: "PaymentTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_PaymentTypeId",
+                table: "Payment",
+                column: "PaymentTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MeterMeasurement");
+
+            migrationBuilder.DropTable(
+                name: "Payment");
+
+            migrationBuilder.DropTable(
+                name: "UserNotification");
+
+            migrationBuilder.DropTable(
+                name: "UserSetting");
+
+            migrationBuilder.DropTable(
+                name: "MeterMeasurementType");
+
+            migrationBuilder.DropTable(
+                name: "PaymentType");
+        }
+    }
+}
